d2556a12b7c0f33107bc6e60a2ff8e490b12094b
Dong Jin <ceciliajin2019@gmail.com>
2025-01-23 14:15:26 +0800

    changed prompts

--- app/components/PromptSuggestionsRow.tsx
+++ app/components/PromptSuggestionsRow.tsx
@@ -6,7 +6,7 @@ const PromptSuggestionsRow = ({ onPromptClick }) => {
   const prompts = [
     // "Who is head of racing for Aston Martin's F1 Academy team?",
     "Who is the current Formula One World Driver's Champion?",
-    "Who is the highest paid F1 driver?",
+    // "Who is the highest paid F1 driver?",
     "Who will be the newest driver for Ferrari?",
   ]
 
